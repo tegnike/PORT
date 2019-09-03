ca5322e0eba5760768c568641ea3261ec27691bd
tegnike <user@mbp.local>
2019-09-03 13:45:42 +0200

    add static_pages_controller

---
+++ spec/requests/static_pages_controller_spec.rb
@@ -0,0 +1,38 @@
+require "rails_helper"
+
+RSpec.describe StaticPagesController, type: :request do
+  describe "GET #home" do
+    it "returns http success" do
+      get :home
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #help" do
+    it "returns http success" do
+      get :help
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #about" do
+    it "returns http success" do
+      get :about
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #policy" do
+    it "returns http success" do
+      get :policy
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe "GET #privacy" do
+    it "returns http success" do
+      get :privacy
+      expect(response).to have_http_status(:success)
+    end
+  end
+end
