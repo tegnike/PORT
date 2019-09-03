9358a484f0da4ca9c8bd8768770383439fc27e5c
tegnike <user@mbp.local>
2019-09-03 22:14:28 +0200

    fix spec of static_pages_controller

--- spec/requests/static_pages_controller_spec.rb
+++ spec/requests/static_pages_controller_spec.rb
@@ -1,38 +1,43 @@
 require "rails_helper"
 
 RSpec.describe StaticPagesController, type: :request do
-  describe "GET #home" do
+  describe "GET #root" do
     it "returns http success" do
-      get :home
+      get root_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "#{full_title("")}"
     end
   end
 
   describe "GET #help" do
     it "returns http success" do
-      get :help
+      get help_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "#{full_title("Help")}"
     end
   end
 
   describe "GET #about" do
     it "returns http success" do
-      get :about
+      get about_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "#{full_title("About")}"
     end
   end
 
   describe "GET #policy" do
     it "returns http success" do
-      get :policy
+      get policy_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "#{full_title("Policy")}"
     end
   end
 
   describe "GET #privacy" do
     it "returns http success" do
-      get :privacy
+      get privacy_path
       expect(response).to have_http_status(:success)
+      expect(response.body).to include "#{full_title("Privacy")}"
     end
   end
 end
