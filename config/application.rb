cfff8f8dc6ef0030f17a2cac5f3502eca7fd673e
tegnike <user@mbp.local>
2019-09-03 10:29:21 +0200

    set redis

--- config/application.rb
+++ config/application.rb
@@ -29,5 +29,8 @@ module App
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    # Redis
+    config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
   end
 end
