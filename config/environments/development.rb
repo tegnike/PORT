5727ad0bcaabad0f2b613865551c7559d8eb84d3
tegnike <user@mbp.local>
2019-09-06 13:33:57 +0200

    set email sending

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -63,4 +63,14 @@ Rails.application.configure do
 
   # mailer setting
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    user_name: ENV["GMAIL_ADDRESS"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 end
