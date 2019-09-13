0ae3bc9731d99dda455a6af71d747d8a3d7c8a29
tegnike <user@mbp.local>
2019-09-13 23:17:37 +0200

    add follow/unfollow test to user_model

--- spec/factories/relationships.rb
+++ spec/factories/relationships.rb
@@ -1,6 +1,4 @@
 FactoryBot.define do
   factory :relationship do
-    follower_id { 1 }
-    followed_id { 1 }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,4 +13,19 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_length_of(:username).is_at_most(50) }
     it { is_expected.to validate_length_of(:profile).is_at_most(200) }
   end
+
+  describe "follow and unfollow" do
+    let(:user1) { create(:user) }
+    let(:user2) { create(:user) }
+    subject(:follow) { user1.follow(user2) }
+    subject(:unfollow) { user1.unfollow(user2) }
+    it "shows correct follow and unfollow method" do
+      expect(user1.following?(user2)).to be_falsey
+      follow
+      expect(user1.following?(user2)).to be_truthy
+      expect(user2.followers.include?(user1)).to be_truthy
+      unfollow
+      expect(user1.following?(user2)).to be_falsey
+    end
+  end
 end
