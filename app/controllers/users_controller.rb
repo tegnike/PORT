5d5a833b25e30ad99b40eb62f299646f9d8100b7
tegnike <user@mbp.local>
2019-09-14 21:16:09 +0200

    add following/followed pages and these test

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,18 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def following
+    @title = "フォローユーザー"
+    @user  = User.find(params[:id])
+    @users = @user.following
+    render "show_follow"
+  end
+
+  def followers
+    @title = "フォロワー"
+    @user  = User.find(params[:id])
+    @users = @user.followers
+    render "show_follow"
+  end
 end
--- spec/system/following_spec.rb
+++ spec/system/following_spec.rb
@@ -4,8 +4,13 @@ RSpec.describe "Following", type: :system, js: true do
   describe "follow function" do
     let(:user1) { create(:user, email: "user@example.com", password: "password", password_confirmation: "password") }
     let(:user2) { create(:user) }
+    let(:user3) { create(:user) }
+    let(:user4) { create(:user) }
     before {
+      user1.following << user3
+      user1.following << user4
       user2.following << user1
+      user4.following << user1
 
       visit new_user_session_path # log in by user1
       fill_in "Email", with: "user@example.com"
@@ -24,6 +29,27 @@ RSpec.describe "Following", type: :system, js: true do
         sleep 1
         user1.reload
         expect(user1.following).not_to include(user2)
+      end
+    end
+
+    context "access to following page" do
+      before { visit following_user_path(user1) }
+      it "shows correct following information" do
+        expect(user1.following).not_to be_empty
+        expect(page).to have_content user1.following.count.to_s
+        user1.following.each do |user|
+          expect(page).to have_link "#{user.username}"
+        end
+      end
+    end
+    context "access to followers page" do
+      before { visit followers_user_path(user1) }
+      it "shows correct followers information" do
+        expect(user1.followers).not_to be_empty
+        expect(page).to have_content user1.followers.count.to_s
+        user1.followers.each do |user|
+          expect(page).to have_link "#{user.username}"
+        end
       end
     end
   end
