162c666dba18c19dcfbf6bac97837386723caba7
tegnike <user@mbp.local>
2019-09-18 21:27:38 +0200

    update portfolio_form about actiontext

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,6 +1,7 @@
 class Portfolio < ApplicationRecord
   belongs_to :user
   mount_uploader :image, ImageUploader
+  has_rich_text :content
   default_scope -> { order(created_at: :desc) }
   validates :user_id, presence: true
   validates :title, presence: true, length: { maximum: 100 }
