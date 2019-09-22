d91a29e3ccbbb0f0b6e77efab67232385f0bc014
tegnike <user@mbp.local>
2019-09-22 10:59:10 +0200

    fix for review #6 about controller error handling

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -4,20 +4,30 @@ class FavoritesController < ApplicationController
   def create
     @portfolio = Portfolio.find(params[:portfolio_id])
     @user = User.find(params[:user_id])
-    current_user.add_favorite(@portfolio)
     respond_to do |format|
-      format.html { redirect_to @portfolio }
-      format.js
+      if @portfolio && @user
+        current_user.add_favorite(@portfolio)
+        format.html { redirect_to @portfolio }
+        format.js
+      else
+        flash.now[:alert] = "お気に入りを登録できませんでした。"
+        format.js { render :new }
+      end
     end
   end
 
   def destroy
     @portfolio = Favorite.find(params[:id]).portfolio
     @user = User.find(params[:user_id])
-    current_user.remove_favorite(@portfolio)
     respond_to do |format|
-      format.html { redirect_to @portfolio }
-      format.js
+      if @portfolio && @user
+        current_user.remove_favorite(@portfolio)
+        format.html { redirect_to @portfolio }
+        format.js
+      else
+        flash.now[:alert] = "お気に入りを削除できませんでした。"
+        format.js { render :new }
+      end
     end
   end
 end
