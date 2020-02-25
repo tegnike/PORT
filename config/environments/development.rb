fcffcd1a47b49bce683a78eed23e7ddeb4774df0
tegnike <user@mbp.local>
2020-02-25 23:53:06 +0100

    update development env. (add nginx & webpacker container)

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,7 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   config.action_mailer.default_url_options = { host: "localhost", port: 3000 }
+
+  # log output to app container
+  config.logger = Logger.new(STDOUT)
 end
